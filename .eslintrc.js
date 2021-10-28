b87878227ad7cc9dd19a064c9a869b62517ec731
Kirill Tikhonov <lliriq@me.com>
2021-10-28 18:49:10 +0300

    Setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', {
+      ignoreComments: true,
+    }],
+  },
+  plugins: ['jest'],
 };
--- src/lesson_1_1.js
+++ src/lesson_1_1.js
@@ -2,8 +2,7 @@
 программу, которая выводит в консоль произведение
 и сумму этих чисел. */
 export function sumMul(a, b) {
-
-    const sum = a + b;
-    const mul = a * b;
-    return `Сумма = ${sum}; Произведение = ${mul}.`;
+  const sum = a + b;
+  const mul = a * b;
+  return `Сумма = ${sum}; Произведение = ${mul}.`;
 }
