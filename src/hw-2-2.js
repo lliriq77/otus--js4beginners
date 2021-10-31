3a271ecb5161091755098a97f162ae0ce0bb2733
Kirill Tikhonov <lliriq@me.com>
2021-10-31 16:50:28 +0300

    Eslint issues. package.json fix

--- src/hw-2-2.js
+++ src/hw-2-2.js
@@ -3,21 +3,21 @@
 этому числу (1 — январь, 2 — февраль и т.д.). */
 
 export function requestNum() {
-    const num = prompt('Enter number 1-12') - 1;
+  const num = prompt("Enter number 1-12") - 1;
 
-    const arr = [
-        'january',
-        'fabruary',
-        'march',
-        'april',
-        'may',
-        'june',
-        'july',
-        'august',
-        'september',
-        'october',
-        'november',
-        'december',
-    ];
-    console.log(arr[num]);
+  const arr = [
+    "january",
+    "fabruary",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
+  console.log(arr[num]);
 }
