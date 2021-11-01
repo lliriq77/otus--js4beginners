f42dfdc43ceb08f05acf0433aa05faa40bb34d30
Kirill Tikhonov <lliriq@me.com>
2021-11-01 20:08:14 +0300

    hw-6-1.test fix

--- src/hw-6-1.test.js
+++ src/hw-6-1.test.js
@@ -3,19 +3,17 @@ import { createArr } from "./hw-6-1";
 describe("createArr", () => {
   beforeEach(() => {
     jest.spyOn(console, "log");
+    createArr();
   });
   it("returns max, min of Array", () => {
-    createArr();
     expect(console.log).toHaveBeenLastCalledWith(
       'Наибольший элемент а - "1570", наименьший элемент a - "10".'
     );
   });
   it("return sum of all array elements", () => {
-    createArr();
     expect(console.log).toHaveBeenCalledWith(3659);
   });
   it("returns array = array x 2", () => {
-    createArr();
     expect(console.log).toHaveBeenCalledWith(7318);
   });
 });
