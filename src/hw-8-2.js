459519473b0703f42bda8d858f9a910be4ea6df7
Kirill Tikhonov <lliriq@me.com>
2021-11-05 15:21:07 +0300

    hw-8-1 style fix

--- src/hw-8-2.js
+++ src/hw-8-2.js
@@ -1,13 +1,11 @@
-
 /* Пользователь вводит число R. Написать программу,
 которая выведет в консоль длину окружности и
 площадь круга с радиусом R. */
 
 export function count() {
-    const radius = prompt('Введите радиус');
-    const p = (2 * Math.PI * radius).toFixed(0);
-    const s = (Math.PI * (radius ** 2)).toFixed(0);
+  const radius = prompt("Введите радиус");
+  const p = (2 * Math.PI * radius).toFixed(0);
+  const s = (Math.PI * radius ** 2).toFixed(0);
 
-    console.log(`Длина окружности = ${p}ед., площадь круга = ${s}ед. кв.`)
+  console.log(`Длина окружности = ${p}ед., площадь круга = ${s}ед. кв.`);
 }
-
