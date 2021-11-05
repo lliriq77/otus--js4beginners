a0a1d4efa6fa1afc54a5b380f5fe82b975cc8dcf
Kirill Tikhonov <lliriq@me.com>
2021-11-05 21:37:34 +0300

    hw-9-2 fix

--- src/hw-9-2.js
+++ src/hw-9-2.js
@@ -5,6 +5,5 @@ export function minPassedToday() {
   let today = new Date();
   today = new Date(today.getFullYear(), today.getMonth(), today.getDate());
   const minutesPassedToday = ((Date.now() - today) / 1000 / 60).toFixed(0);
-
   console.log(minutesPassedToday);
 }
--- src/hw-9-2.test.js
+++ src/hw-9-2.test.js
@@ -2,10 +2,9 @@ import { minPassedToday } from "./hw-9-2";
 
 describe("minPassedToday", () => {
   it("returns minutes passed from the beiging of 05.11.2021 to 19.00", () => {
-    jest.spyOn(Date.prototype, "getTime").mockReturnValue(1638651600000);
-    jest.spyOn(Date, "now").mockReturnValue(1636128002931);
+    jest.useFakeTimers().setSystemTime(new Date(2021, 11, 5, 19).getTime());
     jest.spyOn(console, "log");
     minPassedToday();
-    expect(console.log).toBeCalledWith("1140");
+    expect(console.log).toHaveBeenCalledWith("1140");
   });
 });
