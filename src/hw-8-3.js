d26f9a7645434345ee069888734b95b1bb2e70c4
Kirill Tikhonov <lliriq@me.com>
2021-11-05 15:49:02 +0300

    Add hw-8-3 fix

--- src/hw-8-3.js
+++ src/hw-8-3.js
@@ -2,22 +2,21 @@
 программу, выводящую корни квадратного
 уравнения с коэффициентами a, b и c. */
 export function rslvQuadEq() {
-    const aT = prompt('Enter a');
-    const bT = prompt('Enter b');
-    const cT = prompt('Enter c');
-    const d = bT ** 2 - 4 * aT * cT;
-    let x1; let x2;
+  const aT = prompt("Enter a");
+  const bT = prompt("Enter b");
+  const cT = prompt("Enter c");
+  const d = bT ** 2 - 4 * aT * cT;
+  let x1;
+  let x2;
 
-    if (d > 0) {
-        x1 = (-bT + (d ** 0.5)) / (2 * aT);
-        x2 = (-bT - (d ** 0.5)) / (2 * aT);
-        return `Ответ: ${x1}; ${x2}.`;
-
-    } if (d === 0) {
-        x1 = -bT / (2 * aT);
-        return `Ответ: ${x1}.`;
-
-    } 
-        return ('Уравение не имеет корней');
-    
+  if (d > 0) {
+    x1 = (-bT + d ** 0.5) / (2 * aT);
+    x2 = (-bT - d ** 0.5) / (2 * aT);
+    return `Ответ: ${x1}; ${x2}.`;
+  }
+  if (d === 0) {
+    x1 = -bT / (2 * aT);
+    return `Ответ: ${x1}.`;
+  }
+  return "Уравение не имеет корней";
 }
